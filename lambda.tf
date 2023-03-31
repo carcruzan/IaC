09a78aefb02eaf3020a2884c1e05144b56b2837d
carcruzan <36182856+carcruzan@users.noreply.github.com>
2023-03-31 11:13:10 -0500

    Create lambda.tf

---
+++ lambda.tf
@@ -0,0 +1,14 @@
+data "archive_file" "demo_lambda_zip" {
+  type        = "zip"
+  source_dir  = "test-function"
+  output_path = "test-function.zip"
+}
+
+resource "aws_lambda_function" "demo_lambda" {
+  filename         = data.archive_file.demo_lambda_zip.output_path
+  source_code_hash = data.archive_file.demo_lambda_zip.output_base64sha256
+  role             = aws_iam_role.demo_lambda.arn
+  function_name    = "demo_lambda"
+  handler          = "handler.lambda_handler"
+  runtime          = "python3.8"
+}
