964ca9125d071a6d5554ba17f3eaddbfeb6b4ad0
carcruzan <36182856+carcruzan@users.noreply.github.com>
2023-03-31 11:37:40 -0500

    Update lambda_function.py

--- test-function/lambda_function.py
+++ test-function/lambda_function.py
@@ -1,5 +1,5 @@
 import requests
 def handler(event, context):   
-    response = requests.get("https://www.github.com/")
+    response = requests.get("https://www.docker.com/")
     print(response.text)
     return response.text
