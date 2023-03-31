9b10f5db400c4fce6c94e19e1cfb8a1a34a360db
carcruzan <36182856+carcruzan@users.noreply.github.com>
2023-03-31 11:32:59 -0500

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -4,6 +4,23 @@ data "archive_file" "demo_lambda_zip" {
   output_path = "test-function.zip"
 }
 
+resource "aws_iam_role" "demo_lambda" {
+  name = "role_demo_lambda"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
 resource "aws_lambda_function" "demo_lambda" {
   filename         = data.archive_file.demo_lambda_zip.output_path
   source_code_hash = data.archive_file.demo_lambda_zip.output_base64sha256
